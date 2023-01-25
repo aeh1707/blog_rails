a43b15d1924b5cba33cd08756d00ad2167967ffd
aeh1707d <abdelhadi.hireche@outlook.com>
2023-01-25 19:35:31 +0100

    Create custom methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
     belongs_to :author, class_name: 'User', foreign_key: 'author_id'
     has_many :comments
     has_many :likes
+
+    def user_posts_counter=(count)
+        author.update_attribute 'posts_counter', count
+    end
+    
+    def recent_comments
+        comments.last(5)
+    end
 end
