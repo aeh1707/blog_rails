fc0b1e33317577306e291207735ee427adf755c8
aeh1707d <abdelhadi.hireche@outlook.com>
2023-01-27 23:37:21 +0100

    Create tests for models

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+    subject { Post.create(title: 'Hello World', text: 'This is the first post of the blog', author_id: 1) }
+    
+    it 'title should be present' do
+        subject.title = nil
+        expect(subject).to_not be_valid
+    end
+    
+    it 'title should not be too long' do
+        subject.title = 'a' * 251
+        expect(subject).to_not be_valid
+    end
+    
+    it 'comments counter should be an integer' do
+        subject.comments_counter = 'one'
+        expect(subject).to_not be_valid
+    end
+    
+    it 'comments counter should be greater than or equal to 0' do
+        subject.comments_counter = -1
+        expect(subject).to_not be_valid
+    end
+    
+    it 'likes counter should be an integer' do
+        subject.likes_counter = 'one'
+        expect(subject).to_not be_valid
+    end
+    
+    it 'likes counter should be greater than or equal to 0' do
+        subject.likes_counter = -1
+        expect(subject).to_not be_valid
+    end
+    
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject {  User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'posts counter should be an integer' do
+    subject.posts_counter = 'one'
+    expect(subject).to_not be_valid
+  end
+
+  it 'posts counter should be greater than or equal to 0' do
+    subject.posts_counter = -1
+    expect(subject).to_not be_valid
+  end
+
+end
