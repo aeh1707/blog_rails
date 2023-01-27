a873b47f5dcced06a1c3a46b414ca40e5347e756
aeh1707d <abdelhadi.hireche@outlook.com>
2023-01-27 23:34:54 +0100

    Refactoring post and user models

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,10 +5,9 @@ class Post < ApplicationRecord
 
   after_save :update_posts_counter
 
-  validates :title, presence: true
-  validates :title, length { maximum: 250 }
-  validates :comments_counter, comparison { greater_than_or_equal_to: 0 }
-  validates :likes_counter, comparison { greater_than_or_equal_to: 0 } 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_posts_counter
     author.increment!(:posts_counter)
