dc39592379fc18df4d1e0a397948ef0537db7c3d
aeh1707d <abdelhadi.hireche@outlook.com>
2023-01-31 19:06:27 +0100

    Correct linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,17 @@
 class PostsController < ApplicationController
-    before_action: fetch_user
+  before_action :fetch_user
 
-    def index
-        @posts = @user.posts.all
-    end
+  def index
+    @posts = @user.posts.all
+  end
 
-    def show
-        @post = @user.find(params[:id])
-    end
+  def show
+    @post = @user.find(params[:id])
+  end
 
-    private
+  private
 
-    def fetch_user
-        @user = User.find(params[:user_id])
-    end
+  def fetch_user
+    @user = User.find(params[:user_id])
+  end
 end
