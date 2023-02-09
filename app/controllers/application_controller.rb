3fa0b4856c86e942982e4dbdaf1bfff3a987ab67
aeh1707d <abdelhadi.hireche@outlook.com>
2023-02-09 23:08:12 +0100

    fix linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,9 @@ class ApplicationController < ActionController::Base
     @users = User.all
   end
 
-
   def update_allowed_parameters
-      devise_parameter_sanitizer.permit(:sign_up) do |u|
-          u.permit(:name, :bio, :photo, :email, :password, :password_confirmation)
-      end
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :bio, :photo, :email, :password, :password_confirmation)
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  # layout 'standard'
   def index
     @users = User.all
   end
